e54e46bed29f05def16089fd5b2d99475d381682
World-X <Mario13X12@gmail.com>
2024-11-14 11:55:28 -0800

    Configurando la base

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,35 @@
 int main()
 {
     //===============> Inicialización
-    InitWindow(1280, 720, "Breakout");
+
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    int ventanaAncho = 1280;
+    int ventanaAlto = 720;
+    InitWindow(ventanaAncho, ventanaAlto, "Breakout");
     SetTargetFPS(60);
+
     //===============> Ciclo principal
+
     while (!WindowShouldClose())
     {
+        //===============> Lógica
+
+        if (IsWindowResized)
+        {
+            ventanaAncho = GetScreenWidth();
+            ventanaAlto = GetScreenHeight();
+        }
+
+        //===============> Dibujo
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("¡Hola, mundo!", 30, 30, 40, BLACK);
         EndDrawing();
     }
+
     //===============> Cierre
+
     CloseWindow();
     return 0;
 }
