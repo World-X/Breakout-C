6a440badb6a3e14d80c4a5cb372a8cef98db919f
World-X <Mario13X12@gmail.com>
2024-11-15 12:19:01 -0800

    Cambios de nombres de variables y constantes

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,19 @@
 
 //==============================> Macros
 
+//===============> Funciones
+
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+//===============> Constantes literales
+
+#define MENU_PRINCIPAL_LADRILLOS_FILAS 5
+#define MENU_PRINCIPAL_LADRILLOS_COLUMNAS 10
+
 //==============================> Variables globales
+
+//===============> Tamaño del juego
 
 int juegoAncho = 1920;
 int juegoAlto = 1080;
@@ -22,15 +31,18 @@ int mitadJuegoAlto = juegoAlto / 2;
 
 int main()
 {
-    //===============> Inicialización de ventana y configuración de raylib
+    //===============> Inicialización de ventana
 
-    SetTraceLogLevel(TraceLogLevel::LOG_TRACE);
     SetConfigFlags(ConfigFlags::FLAG_WINDOW_RESIZABLE | ConfigFlags::FLAG_VSYNC_HINT);
     int ventanaAncho = 1280;
     int ventanaAlto = 720;
     const char *juegoTitulo = "Breakout";
     InitWindow(ventanaAncho, ventanaAlto, juegoTitulo);
     SetTargetFPS(60);
+
+    //===============> Configuración de raylib
+
+    SetTraceLogLevel(TraceLogLevel::LOG_TRACE);
     SetExitKey(KEY_NULL);
 
     //===============> Inicialización de GUI
@@ -54,18 +66,18 @@ int main()
 
     //===============> Variables locales a la función principal
 
-    Color defaultBackgroundColor = (Color){24, 24, 24, 255};
+    const Color defaultBackgroundColor = (Color){24, 24, 24, 255};
 
     RenderTexture2D juegoRenderTextura = LoadRenderTexture(juegoAncho, juegoAlto);
     SetTextureFilter(juegoRenderTextura.texture, TextureFilter::TEXTURE_FILTER_BILINEAR);
 
-    int anchoBotonMenuPrincipal = 420;
-    int altoBotonMenuPrincipal = 100;
-    int posicionYBotonMenuPrincipal = 600;
-    int margenBotonMenuPrincipal = 120;
-    Rectangle botonJugarRect = {mitadJuegoAncho - anchoBotonMenuPrincipal / 2, posicionYBotonMenuPrincipal, anchoBotonMenuPrincipal, altoBotonMenuPrincipal};
-    Rectangle botonLeaderboardRect = {mitadJuegoAncho - anchoBotonMenuPrincipal / 2, posicionYBotonMenuPrincipal + margenBotonMenuPrincipal, anchoBotonMenuPrincipal, altoBotonMenuPrincipal};
-    Rectangle botonSalirRect = {mitadJuegoAncho - anchoBotonMenuPrincipal / 2, posicionYBotonMenuPrincipal + margenBotonMenuPrincipal * 2, anchoBotonMenuPrincipal, altoBotonMenuPrincipal};
+    const int anchoBotonMenuPrincipal = 420;
+    const int altoBotonMenuPrincipal = 100;
+    const int posicionYBotonMenuPrincipal = 600;
+    const int margenBotonMenuPrincipal = 120;
+    const Rectangle botonJugarRect = {mitadJuegoAncho - anchoBotonMenuPrincipal / 2, posicionYBotonMenuPrincipal, anchoBotonMenuPrincipal, altoBotonMenuPrincipal};
+    const Rectangle botonLeaderboardRect = {mitadJuegoAncho - anchoBotonMenuPrincipal / 2, posicionYBotonMenuPrincipal + margenBotonMenuPrincipal, anchoBotonMenuPrincipal, altoBotonMenuPrincipal};
+    const Rectangle botonSalirRect = {mitadJuegoAncho - anchoBotonMenuPrincipal / 2, posicionYBotonMenuPrincipal + margenBotonMenuPrincipal * 2, anchoBotonMenuPrincipal, altoBotonMenuPrincipal};
 
     Pelota pelotaMenuPrincipal = Pelota((Vector2){mitadJuegoAncho, mitadJuegoAlto}, (Vector2){(float)GetRandomValue(300, 420), (float)GetRandomValue(300, 420)}, 20.0f, (Color){200, 200, 200, 255});
 
@@ -76,17 +88,16 @@ int main()
 
     CPUPaleta cpuPaletaMenuPrincipal = CPUPaleta((Vector2){mitadJuegoAncho, juegoAlto - 60.0f}, (Vector2){200.0f, 20.0f}, 420.0f, 4, 2.0f, (Color){186, 225, 255, 255});
 
-    Color ladrillosColores[5] = {(Color){255, 0, 0, 255}, (Color){255, 165, 0, 255}, (Color){255, 255, 0, 255}, (Color){0, 128, 0, 255}, (Color){0, 0, 255, 255}};
-
-    Ladrillo ladrillosMenuPrincipal[5][10];
-    for (int i = 0; i < 5; i++)
+    Color ladrillosColores[MENU_PRINCIPAL_LADRILLOS_FILAS] = {(Color){255, 0, 0, 255}, (Color){255, 165, 0, 255}, (Color){255, 255, 0, 255}, (Color){0, 128, 0, 255}, (Color){0, 0, 255, 255}};
+    const float ladrillosSeparacion = 8.0f;
+    Ladrillo ladrillosMenuPrincipal[MENU_PRINCIPAL_LADRILLOS_FILAS][MENU_PRINCIPAL_LADRILLOS_COLUMNAS];
+    for (int fila = 0; fila < MENU_PRINCIPAL_LADRILLOS_FILAS; fila++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int columna = 0; columna < MENU_PRINCIPAL_LADRILLOS_COLUMNAS; columna++)
         {
-            float separacion = 10.0f;
-            Vector2 tamaño = (Vector2){(juegoAncho - separacion * 11) / 10, 40.0f};
-            Vector2 posicion = (Vector2){j * (tamaño.x + separacion) + separacion, i * (tamaño.y + separacion) + separacion};
-            ladrillosMenuPrincipal[i][j] = Ladrillo(posicion, tamaño, 0.5f, 4, ladrillosColores[i], true, 10 * (5 - i));
+            Vector2 tamaño = (Vector2){(juegoAncho - ladrillosSeparacion * (MENU_PRINCIPAL_LADRILLOS_COLUMNAS + 1)) / MENU_PRINCIPAL_LADRILLOS_COLUMNAS, 40.0f};
+            Vector2 posicion = (Vector2){columna * (tamaño.x + ladrillosSeparacion) + ladrillosSeparacion, fila * (tamaño.y + ladrillosSeparacion) + ladrillosSeparacion};
+            ladrillosMenuPrincipal[fila][columna] = Ladrillo(posicion, tamaño, 0.5f, 4, ladrillosColores[fila], true, 100 * (MENU_PRINCIPAL_LADRILLOS_FILAS - fila));
         }
     }
 
@@ -106,18 +117,23 @@ int main()
 
         //===============> Mouse virtual
 
-        Vector2 mouse = GetMousePosition();
+        Vector2 mouseOffset = {0};
+        mouseOffset.x = (ventanaAncho - (juegoAncho * juegoEscala)) * 0.5f;
+        mouseOffset.y = (ventanaAlto - (juegoAlto * juegoEscala)) * 0.5f;
+        const Vector2 mouse = GetMousePosition();
         Vector2 mouseVirtual = {0};
-        mouseVirtual.x = (mouse.x - (ventanaAncho - (juegoAncho * juegoEscala)) * 0.5f) / juegoEscala;
-        mouseVirtual.y = (mouse.y - (ventanaAlto - (juegoAlto * juegoEscala)) * 0.5f) / juegoEscala;
+        mouseVirtual.x = (mouse.x - mouseOffset.x) / juegoEscala;
+        mouseVirtual.y = (mouse.y - mouseOffset.y) / juegoEscala;
         mouseVirtual = Vector2Clamp(mouseVirtual, (Vector2){0, 0}, (Vector2){(float)juegoAncho, (float)juegoAlto});
-        SetMouseOffset(-(ventanaAncho - (juegoAncho * juegoEscala)) * 0.5f, -(ventanaAlto - (juegoAlto * juegoEscala)) * 0.5f);
+        SetMouseOffset(-mouseOffset.x, -mouseOffset.y);
         SetMouseScale(1 / juegoEscala, 1 / juegoEscala);
 
-        //===============> Lógica
+        //===============> Actualizar lógica del juego
 
         pelotaMenuPrincipal.Actualizar();
         cpuPaletaMenuPrincipal.Actualizar(pelotaMenuPrincipal.posicion);
+
+        //===============> Colisiones
 
         if (CheckCollisionCircleRec(pelotaMenuPrincipal.posicion, pelotaMenuPrincipal.radio, cpuPaletaMenuPrincipal.ObtenerRectangulo()))
         {
@@ -125,9 +141,9 @@ int main()
             pelotaMenuPrincipal.posicion.y += pelotaMenuPrincipal.velocidad.y * GetFrameTime();
         }
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < MENU_PRINCIPAL_LADRILLOS_FILAS; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < MENU_PRINCIPAL_LADRILLOS_COLUMNAS; j++)
             {
                 if (ladrillosMenuPrincipal[i][j].activo)
                 {
@@ -144,10 +160,12 @@ int main()
         //===============> Renderización
 
         BeginTextureMode(juegoRenderTextura);
+
         ClearBackground(defaultBackgroundColor);
-        for (int i = 0; i < 5; i++)
+
+        for (int i = 0; i < MENU_PRINCIPAL_LADRILLOS_FILAS; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < MENU_PRINCIPAL_LADRILLOS_COLUMNAS; j++)
             {
                 if (ladrillosMenuPrincipal[i][j].activo)
                 {
@@ -155,9 +173,12 @@ int main()
                 }
             }
         }
+
         pelotaMenuPrincipal.Dibujar();
         cpuPaletaMenuPrincipal.Dibujar();
+
         DrawText(juegoTitulo, mitadJuegoAncho - MeasureText(juegoTitulo, 160) / 2, 340, 160, WHITE);
+
         if (GuiButton(botonJugarRect, "Jugar"))
         {
             TraceLog(LOG_INFO, "JUGAR");
@@ -170,6 +191,7 @@ int main()
         {
             break;
         }
+
         EndTextureMode();
 
         //===============> Dibujo
